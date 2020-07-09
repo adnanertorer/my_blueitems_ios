0b9c19860015f7bc3783b982452fc609a1d403e3
adnanertorer <adnanertorer@hotmail.com>
2020-07-09 15:58:02 +0300

    Initial Commit

---
+++ Buzzy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Buzzy
+//
+//  Created by Adnan Ertorer on 9.07.2020.
+//  Copyright © 2020 Almula Yazılım. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
